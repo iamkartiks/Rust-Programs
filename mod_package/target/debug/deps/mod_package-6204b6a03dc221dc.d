8e023cc3fe8da775f20b62cd2a2b2babd3469909
iamkartiks <kartikeysinha40@gmail.com>
2023-12-27 14:23:33 +0530

    modules intro

---
+++ mod_package/target/debug/deps/mod_package-6204b6a03dc221dc.d
@@ -0,0 +1,7 @@
+/Users/kartikeysinha/Desktop/local-git-repository/Rust-Programs/mod_package/target/debug/deps/mod_package-6204b6a03dc221dc: src/main.rs src/garden.rs src/garden/vegetable.rs
+
+/Users/kartikeysinha/Desktop/local-git-repository/Rust-Programs/mod_package/target/debug/deps/mod_package-6204b6a03dc221dc.d: src/main.rs src/garden.rs src/garden/vegetable.rs
+
+src/main.rs:
+src/garden.rs:
+src/garden/vegetable.rs:
---
+++ mod_package/target/debug/mod_package.d
@@ -0,0 +1,1 @@
+/Users/kartikeysinha/Desktop/local-git-repository/Rust-Programs/mod_package/target/debug/mod_package: /Users/kartikeysinha/Desktop/local-git-repository/Rust-Programs/mod_package/src/garden/vegetable.rs /Users/kartikeysinha/Desktop/local-git-repository/Rust-Programs/mod_package/src/garden.rs /Users/kartikeysinha/Desktop/local-git-repository/Rust-Programs/mod_package/src/main.rs
